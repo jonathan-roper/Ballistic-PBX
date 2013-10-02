32aba5ecf736595e779cf9c4d7f6fde143eeb931
Jonathan Roper <joe.roper@gmail.com>
2013-10-02 18:48:45 +0200

    Add Sudo to dependencies

--- install-ballistic-pbx.sh
+++ install-ballistic-pbx.sh
@@ -837,6 +837,7 @@ read TEMP
 
 apt-get install openssh-server
 
+
 #check timezone
 dpkg-reconfigure tzdata
 
@@ -849,7 +850,7 @@ apt-get -y install mysql-server
 apt-get -y install mysql-client libmysqlclient-dev build-essential sysvinit-utils libxml2 libxml2-dev libncurses5-dev libcurl4-openssl-dev libvorbis-dev libspeex-dev unixodbc unixodbc-dev libiksemel-dev wget iptables php5 php5-cli php-pear php5-mysql php-db libapache2-mod-php5 php5-gd php5-curl sqlite libnewt-dev libusb-dev zlib1g-dev  libsqlite0-dev  libapache2-mod-auth-mysql sox mpg123 postfix flite php5-mcrypt python-setuptools python-mysqldb python-psycopg2 python-sqlalchemy ntp
 
 #extras 
-apt-get -y install wget iptables vim subversion flex bison libtiff-tools ghostscript autoconf gcc g++ automake libtool patch
+apt-get -y install wget sudo iptables vim subversion flex bison libtiff-tools ghostscript autoconf gcc g++ automake libtool patch
 
 
 
